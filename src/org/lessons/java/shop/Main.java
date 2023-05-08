bc85a78f8e0c664b69a4686596b48c9e3be86f46
PinoMacri <pinonegozio1@outlook.it>
2023-05-08 16:17:01 +0200

    first commit

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.shop;
+
+public class Main {
+	public static void main(String[] args) {
+	Prodotto secchio = new Prodotto ("pino", "bello", 14, 22);
+	 System.out.println(secchio.toString());
+	}
+	
+}
